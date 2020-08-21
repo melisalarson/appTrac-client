aa23377c34ffc2a527293f7b50421a832ae31871
melisalarson <larsonmelisa@gmail.com>
2020-08-21 16:31:00 -0700

    cleaned up files from react. set up nav bar

---
+++ app-trac/src/components/Navbar/Navbar.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "../Navbar/Navbar";
+import "./Navbar.css"
+
+function NavBar() {
+  return (
+    <nav>
+      <div className="container">
+        
+        <NavLink className="logo" to="/">
+          <h1>appTrac</h1>
+        </NavLink>
+
+        <ul className="nav-list">
+          <li className="nav-item">
+            <NavLink className="nav-link" exact to="/companies">Companies</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/login">Login</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/register">Register</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" exact to="/Logout">Logout</NavLink>
+          </li>
+        </ul>
+
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
