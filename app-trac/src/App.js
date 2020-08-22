4152ea19448b3960e1756a86fa072ffccf8e74fb
melisalarson <larsonmelisa@gmail.com>
2020-08-21 17:12:20 -0700

    applications/home page with components and css

--- app-trac/src/App.js
+++ app-trac/src/App.js
@@ -2,11 +2,16 @@ import React from 'react';
 import Navbar from './components/Navbar/Navbar'
 
 import './App.css';
+import Application from './components/Application/Application';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Application />
+      <Company />
+      {/* <Job />
+      <Connection /> */}
     </div>
   );
 }
---
+++ app-trac/src/components/Application/Application.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import "./Application.css";
+
+function Application() {
+  return (
+    <div>
+      <h1>hello</h1>
+
+      <div className="application-card">
+        <section className="content-wrapper">
+          <h4>google</h4>
+          {/* {!list && (
+            <> */}
+          <p>software engineer</p>
+          <p>8/20/20</p>
+          <p>Complete Assignment</p>
+          <p>8/22/20</p>
+          {/* </>
+          )} */}
+        </section>
+      </div>
+    </div>
+  );
+}
+
+export default Application;
