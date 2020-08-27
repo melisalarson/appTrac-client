ab0bb129d30799aaddde43ec96fe3aafa94bd1f3
melisalarson <larsonmelisa@gmail.com>
2020-08-27 10:31:49 -0700

    Updates

---
+++ precache-manifest.1d8e75b97ba1e1fb5f648cb789675d36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88851154c5599b54a55335d1b65f9270",
+    "url": "/appTrac-client/index.html"
+  },
+  {
+    "revision": "96c8040b991a0e5d54fb",
+    "url": "/appTrac-client/static/css/main.f4c95cbb.chunk.css"
+  },
+  {
+    "revision": "2a74f1dfae053d20aa04",
+    "url": "/appTrac-client/static/js/2.584f64e3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/appTrac-client/static/js/2.584f64e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96c8040b991a0e5d54fb",
+    "url": "/appTrac-client/static/js/main.da907d94.chunk.js"
+  },
+  {
+    "revision": "a601bbbf37ed8de1b29d",
+    "url": "/appTrac-client/static/js/runtime-main.0e51caee.js"
+  }
+]);
